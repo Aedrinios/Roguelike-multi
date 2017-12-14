17bd1b7639064173fdc4029b24f1ff6db6bd61f5
Homère <homerebourgeois@hotmail.fr>
2017-12-14 02:16:37 +0100

    ajout porter jeter objets

--- Assets/Script/Character.cs
+++ Assets/Script/Character.cs
@@ -10,12 +10,19 @@ public class Character : AnimateEntity {
 	public ArrayList ground;
 	private Rigidbody2D rigidb;
 	private Animator animator;
+    private float primaryTimer;
+    private float secondaryTimer;
 
-	void Start () {
+    private bool isCarrying = false;
+    private InanimateEntity carriedObject;
+
+    void Start () {
         speed = 10;
         life = 10;
         attack = 1;
-		inventory = new InanimateEntity [2];
+         primaryTimer=0;
+    secondaryTimer=0;
+    inventory = new InanimateEntity [2];
 		ground = new ArrayList();
 		rigidb = this.GetComponent<Rigidbody2D> ();
 		//this.GetComponent<SpriteRenderer>().color = new Color (Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f);
@@ -31,15 +38,40 @@ public class Character : AnimateEntity {
 	}
 
     public void Update() // déséquiper pour l'instant
-    {
-       if(Input.GetKeyDown(KeyCode.T))
-        {
-            inventory[1].Unequip();
-        }    
+    {    
        if(life<=0)
         {
             Debug.Log("YOU DIED!  (git gud)");
         }
+
+        //ramassage puis lancer
+        if (Input.GetButtonDown("Keyboard 2 interact")/*|| Input.GetButtonDown("Keyboard 1 interact")*/)
+        {
+            Debug.Log("gg");
+            Interract();
+        }
+
+        //déséquipement arme 1
+        if (Input.GetButton("Keyboard 2 primary"))
+        {
+            Debug.Log("gg");
+            primaryTimer += Time.deltaTime;
+            if (primaryTimer > 2 && inventory[0] != null)
+                Carry(inventory[0]);
+        }
+        if (Input.GetButtonUp("Keyboard 2 primary"))
+            primaryTimer = 0;
+
+        //desequipement arme 2
+        if (Input.GetButton("Keyboard 2 secondary"))
+        {
+            Debug.Log("gg");
+            secondaryTimer += Time.deltaTime;
+            if (secondaryTimer > 2 && inventory[1]!=null)
+                Carry(inventory[1]);
+        }
+        if (Input.GetButtonUp("Keyboard 2 secondary"))
+            secondaryTimer = 0;
     }
 	public IEnumerator ReceiveHit (){
         if(canBeDamaged)
@@ -76,11 +108,74 @@ public class Character : AnimateEntity {
 		}	//No need to remove the item from the ground list, it's done in OnTriggerExit
 	}
 
-	public void Interract () {
-		//Si un objet interractible est sous ses pieds, alors interragis avec.
-	}
+    private void TryCarry()
+    {
+        if (ground.Count != 0)
+        {
+            carriedObject = (InanimateEntity)ground[0];
+            Carry(carriedObject);
+        }   //No need to remove the item from the ground list, it's done in OnTriggerExit
+    }
 
-	public void Idle() {
+    public void Interract () {
+        //Si un objet interractible est sous ses pieds, alors interragis avec.
+        if (isCarrying)
+        {
+            Throw();
+        }
+        else
+        {
+            TryCarry();
+        }
+    }
+
+    public void Carry(InanimateEntity Ientity)
+    {
+        if(Ientity == inventory[0])
+        {
+            Ientity.Unequip(0);
+        }
+        if (Ientity == inventory[1])
+        {
+            Ientity.Unequip(1);
+        }
+        if (Ientity.tag == "Player" || Ientity.tag == "ennemi")
+        {
+            Ientity.GetComponent<AnimateEntity>().stun = true;
+        }
+        isCarrying = true;
+        Debug.Log("blub");
+        Ientity.GetComponentInChildren<CircleCollider2D>().enabled = false;
+        Ientity.pickupCollider.enabled = false;
+        Ientity.GetComponentInChildren<SpriteRenderer>().enabled = true;
+        Ientity.transform.parent = this.transform;
+        Ientity.transform.localPosition = Vector3.zero;
+        Ientity.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        carriedObject = Ientity;
+    }
+
+    public void Throw()
+    {
+        Debug.Log("bloub");
+        if (isCarrying)
+        {
+            Debug.Log("blib");
+            if (carriedObject.tag == "Player" || carriedObject.tag == "ennemi")
+            {
+                carriedObject.GetComponent<AnimateEntity>().stun = false;
+            }
+            carriedObject.GetComponent<Rigidbody2D>().AddForce(direction.normalized * 50, ForceMode2D.Impulse);
+            //carriedObject.GetComponentInChildren<CircleCollider2D>().enabled = true;
+            carriedObject.pickupCollider.enabled = true;
+            carriedObject.transform.parent = null;
+            //carriedObject.transform.localPosition = Vector3.zero;
+            //carriedObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            carriedObject = null;
+            isCarrying = false;
+        }
+    }
+
+    public override void  Idle() {
 		animator.SetBool ("isMoving", false);
 		rigidb.velocity = Vector3.zero;
 	}
