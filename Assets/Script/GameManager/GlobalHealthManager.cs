1de49938c4995e606f6e40dd789be939133e3193
DESKTOP-L4O1KT8\flouc <florine.carlier17@gmail.com>
2018-02-02 10:01:54 -0600

    Inputs menu game over

--- Assets/Script/GameManager/GlobalHealthManager.cs
+++ Assets/Script/GameManager/GlobalHealthManager.cs
@@ -13,12 +13,7 @@ public class GlobalHealthManager : MonoBehaviour {
     public GameObject gameOver;
     public GameObject quite;
     public GameObject replay;
-    /*
-    private GameObject panel;
-    private GameObject gameOver;
-    private GameObject quite;
-    private GameObject replay;
-    */
+   
     public Sprite notSelectedQuite;
     public Sprite selectedQuite;
     public Sprite notSelectedReplay;
@@ -28,23 +23,10 @@ public class GlobalHealthManager : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        /*
-        panel = GameObject.Find("PanelFlou");
-        quite = GameObject.Find("Quite");*/
-       // gameOver = GameObject.Find("GameOver");
-       // gameOver = GetComponent<Image>();
-        //   replay = GameObject.Find("Replay");
-
-
         panel.gameObject.SetActive(false);
         gameOver.gameObject.SetActive(false);
         quite.gameObject.SetActive(false);
         replay.gameObject.SetActive(false);
-/*           panel.gameObject.SetActive(true);
-          gameOver.gameObject.SetActive(true);
-          quite.gameObject.SetActive(true);
-          replay.gameObject.SetActive(true);
-  */
         numb = 0;
     }
 	
@@ -55,6 +37,9 @@ public class GlobalHealthManager : MonoBehaviour {
         var butT = UnityEngine.Input.GetButtonDown("Keyboard 2 left-selection"); // Bouton Q pour gauche
         var butG = UnityEngine.Input.GetButtonDown("Keyboard 2 right-selection"); // Bouton D pour droite
         var butB = UnityEngine.Input.GetButtonDown("Keyboard 2 start"); // Bouton W pour selectionner
+        var joystick = UnityEngine.Input.GetAxis("Joystick 1 X axis"); // Joystick gauche/droite 
+        var butStart = UnityEngine.Input.GetButtonDown("Joystick 1 start"); // Bouton start manette
+        var butA = UnityEngine.Input.GetButtonDown("Joystick 1 primary"); // Bouton A manette
 
         if (globalHealth == 0)
         {
@@ -64,23 +49,23 @@ public class GlobalHealthManager : MonoBehaviour {
             quite.gameObject.SetActive(true);
             replay.gameObject.SetActive(true);
 
-            if (butT) // gauche
+            if (butT || joystick < 0) // gauche
             {
                 numb = 0;
                 changeSprite(numb);
             }
-            if (butG) // droite
+            if (butG || joystick > 0) // droite
             {
                 numb = 1;
                 changeSprite(numb);
             }
 
-            if (numb == 0 && butB) // Selection de play
+            if (numb == 0 && butB || butStart || butA) // Selection de play
             {
                 SceneManager.LoadScene("BastienTest");
 
             }
-            if (numb == 1 && butB) // Selection de quite
+            if (numb == 1 && butB || butStart || butA) // Selection de quite
             {
                 SceneManager.LoadScene("Menu");
             }
