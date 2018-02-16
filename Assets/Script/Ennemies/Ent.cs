61d4a9dbe661f7a96472ca295f7718fc10ac60bd
leomani <xboxlive84320@hotmail.fr>
2018-02-16 17:47:43 -0500

    no message

---
+++ Assets/Script/Ennemies/Ent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ent : MonoBehaviour {
+    private GameObject currentTarget;
+    private List<GameObject> players;
+
+	// Use this for initialization
+	void Start () {
+        players = GameManager.instance.players;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void chooseNewTarget()
+    {
+        int rdm = Mathf.RoundToInt(Random.Range(0, players.Count));
+        currentTarget = players[rdm];
+        Debug.Log(currentTarget);
+    }
+}
