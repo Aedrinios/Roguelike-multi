79c3317e4f27a5f1e8de600f406287d3a18939a3
PC_STIEN\Justine <almes.justine@gmail.com>
2018-03-13 16:41:12 -0400

    Correction warning animator

--- Assets/Script/Objects/AnimateEntity.cs
+++ Assets/Script/Objects/AnimateEntity.cs
@@ -130,22 +130,18 @@ public abstract class AnimateEntity : InanimateEntity
             {
                 case ("Doll(Clone)"):
                     SoundManager.playSound("ghoulDegat1");
-                    other.GetComponent<AnimateEntity>().animator.SetBool("isBeingDammage", true);
                     Debug.Log("DEBUG LOG ANIMATION DAMMAGE !!!!!!!! - Doll");
                     break;
                 case ("Ghoul(Clone)"):
                     SoundManager.playSound("ghoulDegat1");
-                    other.GetComponent<AnimateEntity>().animator.SetBool("isBeingDammage", true);
                     Debug.Log("DEBUG LOG ANIMATION DAMMAGE !!!!!!!! - Ghoul");
                     break;
                 case ("Bouboule(Clone)"):
                     SoundManager.playSound("ghoulDegat1");
-                    other.GetComponent<AnimateEntity>().animator.SetBool("isBeingDammage", true);
                     Debug.Log("DEBUG LOG ANIMATION DAMMAGE !!!!!!!! - Bouboule");
                     break;
                 case ("Ordi(Clone)"):
                     SoundManager.playSound("ghoulDegat1");
-                    other.GetComponent<AnimateEntity>().animator.SetBool("isBeingDammage", true);
                     Debug.Log("DEBUG LOG ANIMATION DAMMAGE !!!!!!!! - Ordi");
                     break;
 
